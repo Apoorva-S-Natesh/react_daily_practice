bc8beb0129f735a36e258182ba63a258a1275f59
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-02-03 05:53:42 +0100

    react routing with Route and Link

--- App.js
+++ App.js
@@ -1,28 +1,36 @@
-import React from "react";
-import Form from "./Form";
-//  import { Route, Routes } from 'react-router-dom';
-//  import Home from "./components/Home";
-//  import About from "./components/About";
-//  import Contact from "./components/Contact";
-// //import {Greeting} from "./Greeting"
-
-export default function App() {
-   return (
-	   <>
-		   <Form />
-	   </>
-   )
-}
+import { Route, Routes, Link } from 'react-router-dom';
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import './components/style.css'
+//import {Greeting} from "./Greeting"
+//  import React from "react";
+//  import Form from "./Form";
 
 // export default function App() {
 // 	return (
 // 		<>
-// 		<h1>Learning Routing (/home, /about /contact)</h1>
-// 		<Routes>
-// 		<Route path="/home" element={<Home/>} />
-// 		<Route path="/about" element={<About/>} />
-// 		<Route path="/contact" element={<Contact/>} />
-// 		</Routes>
+// 			<Form />
 // 		</>
 // 	)
 // }
+
+export default function App() {
+	return (
+		<>
+		<h1 className='mainheading'>Learning Routing (/home, /about /contact)</h1>
+		{/* Navigation Links */}
+		<div className='navbar'>
+		<Link to='/'><button>Home</button></Link>
+		<Link to='/about'><button>About</button></Link>
+		<Link to='/contact'><button>Contact</button></Link>
+		</div>
+		<Routes>
+		<Route path="/" element={<Home/>} />
+		<Route path="/about" element={<About/>} />
+		<Route path="/contact" element={<Contact/>} />
+		</Routes>
+
+		</>
+	)
+}
---
+++ Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+
+export default function Home() {
+	return (
+		<>
+		<h2>Home Component</h2>
+		<p className="para">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta tortor leo, ac ornare quam aliquet eu. Mauris consequat sem vel metus commodo, id tristique nisi vestibulum. Phasellus id molestie lectus, vitae vulputate nulla. Sed molestie finibus dapibus. Sed sagittis ultricies tellus, ut dignissim dolor sagittis id. Donec maximus magna.</p>
+		</>
+	)
+}
